dfe14010dcb5d04a40d27c608a007b9ed05c9187
dulumary <lecture@hagulu.com>
2025-01-21 18:25:01 +0900

    [김인규]  - aws 배포 코드 적용

--- src/main/java/com/marondal/marondalgram/MarondalgramApplication.java
+++ src/main/java/com/marondal/marondalgram/MarondalgramApplication.java
@@ -2,11 +2,12 @@ package com.marondal.marondalgram;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
-public class MarondalgramApplication {
+public class MarondalgramApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MarondalgramApplication.class, args);
--- src/main/java/com/marondal/marondalgram/common/FileManager.java
+++ src/main/java/com/marondal/marondalgram/common/FileManager.java
@@ -10,7 +10,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 public class FileManager {
 	
-	public final static String FILE_UPLOAD_PATH = "E:\\dulumaryT\\web\\20240729\\springProject\\upload\\marondalgram"; 
+	public final static String FILE_UPLOAD_PATH = "/home/ec2-user/upload/marondalgram"; 
 
 	public static String saveFile(int userId, MultipartFile file) {
 		
